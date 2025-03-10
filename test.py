9c51bbade32a5477ce1304365b0c4466ddb5c189
Androidest <stcoda@outlook.com>
2025-03-10 09:01:35 +0800

    demand#1: add proxy arg

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 if __name__ == '__main__':
     # parse args
     args = arg_parser.parse_args()
+    set_proxy(args)
+    
     print(f"========= Importing model: {args.model} ===========")
 
     # import models dynamically
--- utils/common.py
+++ utils/common.py
@@ -4,18 +4,20 @@ import random
 import numpy as np
 import argparse
 
-# use proxy if needed
-use_proxy = False
-if use_proxy:
-    proxy_url = '127.0.0.1:1081'
-    os.environ['HTTP_PROXY'] = f'http://{proxy_url}'
-    os.environ['HTTPS_PROXY'] = f'http://{proxy_url}'
-    print(f"Using proxy on: {proxy_url}")
-
 # common argument parser for entry scripts
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--model', type=str, help='choose a model', default='bert_opt') # default is the bert_opt model
 arg_parser.add_argument('--suffix', type=str, help='model save file suffix', default='')
+arg_parser.add_argument('--proxy', type=str, help='use proxy', const='http://127.0.0.1:1081', default=None, nargs='?')
+
+def set_proxy(args):
+    if args.proxy is None:
+        return
+    
+    proxy_url = args.proxy
+    os.environ['HTTP_PROXY'] = proxy_url
+    os.environ['HTTPS_PROXY'] = proxy_url
+    print(f"Using proxy on: {proxy_url}")
 
 def set_seed(seed: int):
     np.random.seed(seed)
